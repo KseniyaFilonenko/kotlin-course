bbcd40de24920df0efc033746da2f6df174e90f2
kfilonenko <filonenko@tut.by>
2025-09-13 13:02:27 +0300

    Homework4 updated

---
+++ src/main/kotlin/lessonsTest/lesson03Test/lesson4.kt
@@ -0,0 +1,30 @@
+package lessonsTest.lesson03Test
+
+class lesson4 {
+    val myInt: Int = 1
+    val myFloat: Float = 0.33F
+    val myLong: Long = 1_23_55L
+    val myShort: Short = 3200
+    val myByte: Byte = 120
+    val  myDouble: Double = 1.2
+    val myBoolean: Boolean = true
+    val myChar: Char = 'c'
+
+    val numbers: Array<Int> = arrayOf(1,2,3)
+    val letters: List<String> = listOf("a","x","c")
+    val double: Set<Double> = setOf(1.0, 2.2)
+
+    val s1: Byte = 100
+    val s2: Long = 156L
+    val s3: Float= 12.34f
+    val s4: Double = 123.456
+    val s5: String = "Hello, World!"
+    val s6: Boolean = true
+    val s7: Char = 'a'
+    val s8: Int = 0
+    val s9: List<Int> = listOf(2, 3, 4)
+    val s10: Set<Double> = setOf(3.14, 45.0)
+    val s11: Array<String> = arrayOf("Hello", "world")
+    val s12: Map<Int, String> = mapOf(12 to "Kotlin")
+    val s13: Map<String, Any> = mapOf("a" to 'a', "b" to true)
+}
